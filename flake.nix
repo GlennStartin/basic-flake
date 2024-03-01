64c9c0be4bbf9bd68cdf2043dba640949aedc64d
Glenn Startin <glenn.startin@gmail.com>
2024-03-01 14:45:39 +0000

    initial home manager config

--- flake.nix
+++ flake.nix
@@ -2,10 +2,15 @@
   description = "My first flake";
 
   inputs = {
+    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { self, nixpkgs, home-manager, ... }: 
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
@@ -14,6 +19,13 @@
       nixos-glenn = lib.nixosSystem {
 	modules = [./nixos/configuration.nix ];
 	};
+    };
+
+    homeConfigurations = {
+       "glenn@nixos-glenn" = home-manager.lib.homeManagerConfiguration {
+         pkgs = nixpkgs.legacyPackages.${system};
+	 modules = [./home-manager/home.nix];
+       };
     };
   };
 }
