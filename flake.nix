c49fb11665876cb1b2c7e8273f8e526689683199
Glenn Startin <glenn.startin@gmail.com>
2024-03-01 11:49:06 +0000

    Move configuratuion.nix to nixos directory and revert to basic flake

--- flake.nix
+++ flake.nix
@@ -1,41 +1,19 @@
 {
-  description = "my epic vims collection";
+  description = "My first flake";
 
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
-    #nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-  nixosConfigurations = {
-   nixos-glenn = nixpkgs.lib.nixosSystem {
-    specialArgs = { inherit system; };
-
-    modules = [
-     ./nixos/configurations.nix
-     ];
-
-   };
-   };
-    devShells.${system}.default =
-      pkgs.mkShell
-        {
-          buildInputs = [
-	    pkgs.neovim
-	    pkgs.vim
-	    ];
-
-	    shellHook = ''
-	      echo "hello glenn"
-	    '';
-        };
-
-
+  outputs = { self, nixpkgs, ... }: 
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+    in {
+    nixosConfigurations = {
+      nixos-glenn = lib.nixosSystem {
+	modules = [./nixos/configuration.nix ];
+	};
+    };
   };
 }
